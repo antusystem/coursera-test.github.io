54325664ae6bb13f7d4d1852a00def4fd2f026ae
antusystem <aleantunes95@gmail.com>
2020-09-04 12:00:32 -0400

    Agregado casi todo, le falta el scroll en xs y el titulo a la izquierda

---
+++ site/scroll.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header-nav").style.top = "0";
+  } else {
+    document.getElementById("header-nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
