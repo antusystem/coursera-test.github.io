978f1026054a1c09ccb0a59c50a6af2e3515a77a
antusystem <aleantunes95@gmail.com>
2020-09-06 10:20:05 -0400

    Completo el assigment3, falta lo opcional

--- mod3_solution/js/scroll.js
+++ mod3_solution/js/scroll.js
@@ -4,7 +4,7 @@ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header-nav").style.top = "0";
   } else {
-    document.getElementById("header-nav").style.top = "-50px";
+    document.getElementById("header-nav").style.top = "-65px";
   }
   prevScrollpos = currentScrollPos;
 }
